83c259607855de84c817c45c13060a153bdc1a87
Thálisson Sinézio <tms.working@gmail.com>
2022-01-27 18:54:31 -0300

    feat(category): Add the route to insert and list

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,28 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
-    name,
-    description,
-    created_at: new Date(),
-  };
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: 'Category already exists!' });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const allCategories = categoriesRepository.list();
+
+  return response.json(allCategories);
 });
 
 export { categoriesRoutes };
